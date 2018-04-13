362bab90fc396edcedc8d4fa6e3ab2958a261733
Tomi Chen <32943907+tctree333@users.noreply.github.com>
2018-04-12 18:30:46 -0700

    Update fan.sh

--- fan.sh
+++ fan.sh
@@ -2,25 +2,28 @@ ARG=$1
 TEMP=$(/opt/vc/bin/vcgencmd measure_temp)
 echo $TEMP
 
+if [ ! -d /sys/class/gpio/gpio18 ]; then
 # Sets pin 18 as an output
 echo "18" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio18/direction
 STATE=$(sudo cat /sys/class/gpio/gpio18/value)
+fi
 
-if [ "$ARG" == "on" ]
+if [[ $ARG == "on" ]]
 then
 echo "1" > /sys/class/gpio/gpio18/value
 echo Fan Turned On
-elif [ "$ARG" == "off" ]
+elif [[ $ARG == "off" ]]
 then
 echo "0" > /sys/class/gpio/gpio18/value
 echo Fan Turned Off
-elif [ "$ARG" == "" ]
+elif [[ $ARG == "" ]]
 then
-    if [ $STATE == 1 ]
+STATE=$(sudo cat /sys/class/gpio/gpio18/value)
+    if [[ $STATE == 1 ]]
     then
     echo Fan Is On
-    elif [ $STATE == 0 ]
+    elif [[ $STATE == 0 ]]
     then
     echo Fan Is Off
     else
@@ -30,4 +33,8 @@ else
 echo Invalid Argument $ARG
 fi
 
+STATE=$(sudo cat /sys/class/gpio/gpio18/value)
+if [[ $STATE == 0 ]]
+then
 echo "18" > /sys/class/gpio/unexport
+fi
